98bf8240d4bfa71c7c882228b434d1559fd5e973
chak8x8 <chak8x8@gmail.com>
2025-04-17 21:33:47 +1000

    0. Const or let?

--- ES6_basic/0-constants.js
+++ ES6_basic/0-constants.js
@@ -1,14 +1,20 @@
 export function taskFirst() {
-    const task = 'I prefer const when I can.';
+    // Use const here, indent with 2 spaces
+    var task = 'I prefer const when I can.';
+    // Indent with 2 spaces
     return task;
 }
 
 export function getLast() {
+    // Indent with 2 spaces
     return ' is okay';
 }
 
 export function taskNext() {
-    let combination = 'But sometimes let';
+    // Use let here, indent with 2 spaces
+    var combination = 'But sometimes let';
+    // Indent with 2 spaces
     combination += getLast();
+    // Indent with 2 spaces
     return combination;
 }
