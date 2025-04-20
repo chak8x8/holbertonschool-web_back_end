2e0a924a34746765fed402f9f78081d5d2941fb4
chak8x8 <chak8x8@gmail.com>
2025-04-20 14:48:35 +1000

    5. A Building

--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -1,9 +1,9 @@
 export default class Building {
   constructor(sqft) {
-    if (typeof sqft !== 'number') throw new TypeError('SQFT must be a number.');
-    //if (this.constructor === Building) throw new Error('Cannot instantiate abstract class');
-    if (this.evacuationWarningMessage === undefined) throw new Error('Class extending Building must override evacuationWarningMessage');
-
+    if (typeof sqft !== 'number') throw new TypeError('Square feet must be a number');
+    if (typeof this.evacuationWarningMessage !== 'function') {
+      throw new Error('Class extending Building must override evacuationWarningMessage');
+    }
     this._sqft = sqft;
   }
 
