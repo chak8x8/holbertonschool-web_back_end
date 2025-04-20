ff72523c2b784c8b8a27b184dc299c91e527785b
chak8x8 <chak8x8@gmail.com>
2025-04-20 15:50:51 +1000

    9. Hoisting

--- ES6_classes/9-hoisting.js
+++ ES6_classes/9-hoisting.js
@@ -4,22 +4,15 @@ export class HolbertonClass {
     this._location = location;
   }
 
-  get year() {
-    return this._year;
-  }
-
-  get location() {
-    return this._location;
-  }
+  get year() { return this._year; }
+  get location() { return this._location; }
 }
 
 export class StudentHolberton {
   constructor(firstName, lastName, holbertonClass) {
     if (typeof firstName !== 'string') throw new TypeError('firstName must be a string');
     if (typeof lastName !== 'string') throw new TypeError('lastName must be a string');
-    if (!(holbertonClass instanceof HolbertonClass)) {
-      throw new TypeError('holbertonClass must be an instance of HolbertonClass');
-    }
+    if (!(holbertonClass instanceof HolbertonClass)) throw new TypeError('holbertonClass must be an instance of HolbertonClass');
 
     this._firstName = firstName;
     this._lastName = lastName;
@@ -39,7 +32,6 @@ export class StudentHolberton {
   }
 }
 
-// Instantiate classes and export list
 const class2019 = new HolbertonClass(2019, 'San Francisco');
 const class2020 = new HolbertonClass(2020, 'San Francisco');
 
@@ -49,6 +41,4 @@ const student3 = new StudentHolberton('Albert', 'Clinton', class2019);
 const student4 = new StudentHolberton('Donald', 'Bush', class2019);
 const student5 = new StudentHolberton('Jason', 'Sandler', class2019);
 
-const listOfStudents = [student1, student2, student3, student4, student5];
-
-export default listOfStudents;
+export const listOfStudents = [student1, student2, student3, student4, student5];
