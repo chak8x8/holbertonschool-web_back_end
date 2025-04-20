2ec043b0bc8afb7d9d33a4a90a210d8a9d035584
chak8x8 <chak8x8@gmail.com>
2025-04-20 15:40:49 +1000

    9. Hoisting

--- ES6_classes/9-hoisting.js
+++ ES6_classes/9-hoisting.js
@@ -28,11 +28,11 @@ export class StudentHolberton {
   }
 
   get fullStudentDescription() {
-    return `${this._firstName} ${this._lastName} - ${this._holbertonClass.year} - ${this._holbertonClass.location}`
+    return `${this._firstName} ${this._lastName} - ${this._holbertonClass.year} - ${this._holbertonClass.location}`;
   }
 }
 
-
+// Instantiate classes and export list
 const class2019 = new HolbertonClass(2019, 'San Francisco');
 const class2020 = new HolbertonClass(2020, 'San Francisco');
 
@@ -42,5 +42,4 @@ const student3 = new StudentHolberton('Albert', 'Clinton', class2019);
 const student4 = new StudentHolberton('Donald', 'Bush', class2019);
 const student5 = new StudentHolberton('Jason', 'Sandler', class2019);
 
-// Instantiate classes and create listOfStudents here
 export const listOfStudents = [student1, student2, student3, student4, student5];
