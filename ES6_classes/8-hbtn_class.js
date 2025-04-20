b5479930a90bf68c3d15c263785c0710361cc0d2
chak8x8 <chak8x8@gmail.com>
2025-04-20 15:25:00 +1000

    8. Primitive - Holberton Class

--- ES6_classes/8-hbtn_class.js
+++ ES6_classes/8-hbtn_class.js
@@ -10,5 +10,7 @@ export default class HolbertonClass {
   [Symbol.toPrimitive](hint) {
     if (hint === 'number') return this._size;
     if (hint === 'string') return this._location;
+
+    return this._location;
   }
 }
