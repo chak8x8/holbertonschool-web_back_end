f54f6a4acbcbb5213653381ed3729d9e00fc69e9
chak8x8 <chak8x8@gmail.com>
2025-04-20 16:02:56 +1000

    10. Vroom

--- ES6_classes/10-car.js
+++ ES6_classes/10-car.js
@@ -1,8 +1,14 @@
 export default class Car {
   constructor(brand, motor, color) {
-    if (typeof brand !== 'string') throw new TypeError('Brand must be a string');
-    if (typeof motor !== 'string') throw new TypeError('Motor must be a string');
-    if (typeof color !== 'string') throw new TypeError('Color must be a string');
+    if (brand !== undefined && typeof brand !== 'string') {
+      throw new TypeError('Brand must be a string');
+    }
+    if (motor !== undefined && typeof motor !== 'string') {
+      throw new TypeError('Motor must be a string');
+    }
+    if (color !== undefined && typeof color !== 'string') {
+      throw new TypeError('Color must be a string');
+    }
 
     this._brand = brand;
     this._motor = motor;
@@ -10,6 +16,6 @@ export default class Car {
   }
 
   cloneCar() {
-    return new this.constructor();
+    return new this.constructor(); // returns instance with undefined attributes
   }
 }
