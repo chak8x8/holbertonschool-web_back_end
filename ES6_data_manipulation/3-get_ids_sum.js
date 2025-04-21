06a6d8e19fb084ec22b7a4f160e5562159521664
chak8x8 <chak8x8@gmail.com>
2025-04-21 18:24:26 +1000

    3. Reduce

--- ES6_data_manipulation/3-get_ids_sum.js
+++ ES6_data_manipulation/3-get_ids_sum.js
@@ -1,6 +1,6 @@
 export default function getStudentIdsSum(students) {
   if (!Array.isArray(students)) throw new TypeError('Students must be an array');
 
-  const sum = students.reduce((acc, student) => acc + student.id, 0)
+  const sum = students.reduce((acc, student) => acc + student.id, 0);
   return sum;
 }
