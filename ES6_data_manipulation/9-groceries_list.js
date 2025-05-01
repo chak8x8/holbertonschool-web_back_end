e1e8114ba456bfe020207d18ce2180775f36411c
chak8x8 <chak8x8@gmail.com>
2025-05-01 17:18:30 +1000

    9. Map data structure

--- ES6_data_manipulation/9-groceries_list.js
+++ ES6_data_manipulation/9-groceries_list.js
@@ -1,11 +1,11 @@
 export default function groceriesList() {
-    // Create and return a Map with the specified groceries
-    const newMap = new Map([
-        ['Apples', 10],
-        ['Tomatoes', 10],
-        ['Pasta', 1],
-        ['Rice', 1],
-        ['Banana', 5],
-    ])
-    return newMap;
+  // Create and return a Map with the specified groceries
+  const newMap = new Map([
+    ['Apples', 10],
+    ['Tomatoes', 10],
+    ['Pasta', 1],
+    ['Rice', 1],
+    ['Banana', 5],
+  ])
+  return newMap;
 }
