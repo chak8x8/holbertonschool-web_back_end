8bac9cba85d3fcde83c6442fb1c2f3da8f502650
chak8x8 <chak8x8@gmail.com>
2025-05-01 17:29:21 +1000

    10. More map data structure

---
+++ ES6_data_manipulation/10-update_uniq_items.js
@@ -0,0 +1,14 @@
+export default function updateUniqueItems(map) {
+  // Check if argument is a Map
+  if (!(map instanceof Map)) {
+    throw new Error('Cannot process');
+  }
+  // Update quantities of 1 to 100
+  map.forEach((value, key) => {
+    if (value === 1) {
+      map.set(key, 100);
+    }
+  })
+  // Return the updated Map
+  return map;
+}
