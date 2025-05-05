5523d284a5b76ce759aa60c02fed26753d1e001e
chak8x8 <chak8x8@gmail.com>
2025-05-05 14:02:54 +1000

    1. Basic annotations - concat

--- python_variable_annotations/1-concat.py
+++ python_variable_annotations/1-concat.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-"""This module contains a type-annotated function to concatenate two strings."""
+"""Module to concatenate two strings."""
 
 
 def concat(str1: str, str2: str) -> str:
