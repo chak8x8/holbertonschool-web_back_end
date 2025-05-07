d757743149da84dd31df2ed150d1302d81f47df3
chak8x8 <chak8x8@gmail.com>
2025-05-07 17:44:56 +1000

    0. The basics of async

--- python_async_function/0-basic_async_syntax.py
+++ python_async_function/0-basic_async_syntax.py
@@ -1,11 +1,10 @@
 #!/usr/bin/env python3
+import asyncio
+import random
 """
 Module for basic async coroutine.
 """
 
-
-import asyncio
-import random
 
 async def wait_random(max_delay: int = 10) -> float:
     """
