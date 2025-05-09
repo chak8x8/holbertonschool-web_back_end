0797dbd8dcd9977256d4e5927499ab9e0c9054f0
chak8x8 <chak8x8@gmail.com>
2025-05-09 17:18:04 +1000

    0. Async Generator

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -10,7 +10,8 @@ from typing import Generator
 
 async def async_generator() -> Generator[float, None, None]:
     """
-    Coroutine that yields 10 random numbers between 0 and 10, each after a 1-second delay.
+    Coroutine that yields 10 random numbers between 0 and 10, 
+    each after a 1-second delay.
 
     Yields:
         float: Random number between 0 and 10.
