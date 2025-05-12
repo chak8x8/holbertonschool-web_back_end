0e496f0d0ad301926852cb09d5e954ba3d439202
chak8x8 <chak8x8@gmail.com>
2025-05-12 21:08:31 +1000

    11. Where can I learn Python?

--- NoSQL/11-schools_by_topic.py
+++ NoSQL/11-schools_by_topic.py
@@ -12,4 +12,4 @@ def schools_by_topic(mongo_collection, topic):
     Returns:
         List of documents with the specified topic
     """
-    return mongo_collection.find({"topic": topic})
+    return list(mongo_collection.find({"topic": topic}))
