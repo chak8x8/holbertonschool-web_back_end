b30abe9829279c4e92b4ae7c96ecdca91941041f
chak8x8 <chak8x8@gmail.com>
2025-05-19 17:53:25 +1000

    3. Handle multiple successful promises

--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -3,11 +3,9 @@ import { uploadPhoto, createUser } from './utils';
 export default function handleProfileSignup() {
   Promise.all([uploadPhoto(), createUser()])
     .then((results) => {
-      // TODO: Log body firstName lastName from results
       console.log(`${results[0].body} ${results[1].firstName} ${results[1].lastName}`);
     })
     .catch(() => {
-      // TODO: Log error message
       console.log('Signup system offline');
     });
 }
