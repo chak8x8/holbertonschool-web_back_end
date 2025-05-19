36c7450a6b1733a8575e034be7f65a727db5eb8e
chak8x8 <chak8x8@gmail.com>
2025-05-19 22:53:18 +1000

    6. Handle multiple promises

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -2,7 +2,9 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
+  return Promise.allSettled([
+    signUpUser(firstName, lastName),
+    uploadPhoto(fileName)])
     .then((results) => {
       return results.map((result) => ({
         status: result.status,
