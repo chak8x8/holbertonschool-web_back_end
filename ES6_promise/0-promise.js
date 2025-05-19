731020a104318f4aa68b396429d951a6b2681ba1
chak8x8 <chak8x8@gmail.com>
2025-05-19 15:08:21 +1000

    0. Keep every promise you make and only make promises you can keep

--- ES6_promise/0-promise.js
+++ ES6_promise/0-promise.js
@@ -1,6 +1,6 @@
 export default function getResponseFromAPI() {
     const myPromise = new Promise((resolve) => {
-        resolve("success");
+        resolve('success');
     });
     return myPromise;
 }
