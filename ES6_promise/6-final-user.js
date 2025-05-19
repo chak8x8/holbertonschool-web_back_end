f958ed2e1179f20b554ae555d98d7a74f19936e8
chak8x8 <chak8x8@gmail.com>
2025-05-19 22:55:22 +1000

    6. Handle multiple promises

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -6,7 +6,7 @@ export default function handleProfileSignup(firstName, lastName, fileName) {
     signUpUser(firstName, lastName),
     uploadPhoto(fileName)])
     .then((results) => {
-      results.map((result) => ({
+      return results.map((result) => ({
         status: result.status,
         value: result.status === 'fulfilled'
           ? result.value
