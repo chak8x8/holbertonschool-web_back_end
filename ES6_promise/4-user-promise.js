69fe934d5db38747aadf9c26e327172596880b85
chak8x8 <chak8x8@gmail.com>
2025-05-19 18:19:47 +1000

    4. Simple promise

--- ES6_promise/4-user-promise.js
+++ ES6_promise/4-user-promise.js
@@ -1,4 +1,3 @@
 export default function signUpUser(firstName, lastName) {
-  // Return a resolved Promise with { firstName, lastName }
-  return Promise.resolve({ firstName, lastName })
+  return Promise.resolve({ firstName, lastName });
 }
