f8b1a7569f9302864b93f5d80be444766bbc4fa0
chak8x8 <chak8x8@gmail.com>
2025-08-11 17:31:14 +1000

    6-set.js

---
+++ ES6_data_manipulation/.eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+    env: {
+        browser: false,
+        es2021: true,
+        node: true,
+    },
+    extends: [
+        'eslint:recommended',
+    ],
+    parserOptions: {
+        ecmaVersion: 12,
+        sourceType: 'module',
+    },
+    rules: {
+        indent: ['error', 2],
+        'eol-last': ['error', 'always'],
+        'no-console': 'off',
+    },
+};
