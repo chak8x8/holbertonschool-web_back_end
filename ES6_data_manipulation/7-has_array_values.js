f8c024daf8ed6cf3ca86b28bcdc5fb0ef29169bb
chak8x8 <chak8x8@gmail.com>
2025-08-11 17:36:08 +1000

    7-has_array_values.js

--- ES6_data_manipulation/7-has_array_values.js
+++ ES6_data_manipulation/7-has_array_values.js
@@ -1,15 +1,14 @@
+/**
+ * Checks if all elements in an array exist within a set.
+ * @param {Set} set - The set to check against.
+ * @param {Array} array - The array of elements to verify.
+ * @returns {boolean} True if all array elements are in the set, false otherwise.
+ */
 export default function hasValuesFromArray(set, array) {
-  // Check if all array elements exist in the set
   for (const value of array) {
     if (!set.has(value)) {
       return false;
     }
   }
   return true;
-
-
-  // alternative solution:
-  //export default function hasValuesFromArray(set, array) {
-  //return array.every((value) => set.has(value));
-  //}
 }
