f429d975b92a53435f0345e2f954973ffcc45d1d
chak8x8 <chak8x8@gmail.com>
2025-08-18 19:34:13 +1000

    1-stdin.js

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,6 +1,7 @@
 console.log('Welcome to Holberton School, what is your name?');
 
 process.stdin.setEncoding('utf8');
+process.stdin.resume();
 
 process.stdin.on('data', (data) => {
     process.stdout.write('Your name is: ' + data.trim() + '\r');
