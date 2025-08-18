5da358e5dea944248db6bd3d930f74d9c8604ef7
chak8x8 <54473526+chak8x8@users.noreply.github.com>
2025-08-18 22:30:08 +1000

    Update 2-read_file.js

--- Node_JS_basic/2-read_file.js
+++ Node_JS_basic/2-read_file.js
@@ -1,28 +1,28 @@
 const fs = require('fs');
 
 function countStudents(path) {
-    try {
-        const data = fs.readFileSync(path, 'utf8');
-        const lines = data.split('\n').slice(1);
-        const groups = { CS: [], SWE: [] };
-        for (const line of lines) {
-            if (line.trim() !== '') {
-                const fields = line.split(',');
-                const firstname = fields[0];
-                const field = fields[3];
-                groups[field].push(firstname);
-            }
-        }
-        const total = groups.CS.length + groups.SWE.length;
-        console.log(`Number of students: ${total}`);
-        for (const field in groups) {
-            if (groups[field].length > 0) {
-                console.log(`Number of students in ${field}: ${groups[field].length}. List: ${groups[field].join(', ')}`);
-            }
-        }
-    } catch (error) {
-        throw new Error('Cannot load the database');
-    }
+  try {
+      const data = fs.readFileSync(path, 'utf8');
+      const lines = data.split('\n').slice(1);
+      const groups = { CS: [], SWE: [] };
+      for (const line of lines) {
+          if (line.trim() !== '') {
+              const fields = line.split(',');
+              const firstname = fields[0];
+              const field = fields[3];
+              groups[field].push(firstname);
+          }
+      }
+      const total = groups.CS.length + groups.SWE.length;
+      console.log(`Number of students: ${total}`);
+      for (const field in groups) {
+          if (groups[field].length > 0) {
+              console.log(`Number of students in ${field}: ${groups[field].length}. List: ${groups[field].join(', ')}`);
+          }
+      }
+  } catch (error) {
+      throw new Error('Cannot load the database');
+  }
 }
 
 module.exports = countStudents;
