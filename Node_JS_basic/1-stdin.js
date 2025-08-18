95c317119c660643a498cc408a2f90bb827d9a2c
chak8x8 <54473526+chak8x8@users.noreply.github.com>
2025-08-18 19:38:22 +1000

    Update 1-stdin.js

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -2,13 +2,10 @@ console.log('Welcome to Holberton School, what is your name?');
 
 process.stdin.setEncoding('utf8');
 
-// No need to .resume(); Node starts reading when listeners are attached
 process.stdin.on('data', (data) => {
-    // Keep the newline from stdin so the output matches exactly:
-    // "Your name is: Sam\n"
-    process.stdout.write(`Your name is: ${data}`);
+  process.stdout.write(`Your name is: ${data}`);
 });
 
 process.stdin.on('end', () => {
-    console.log('This important software is now closing');
+  console.log('This important software is now closing');
 });
