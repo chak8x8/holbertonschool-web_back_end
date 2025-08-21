d2e6cae345f1d404975e94f3f319fef2d588d26e
chak8x8 <54473526+chak8x8@users.noreply.github.com>
2025-08-21 19:12:08 +1000

    Update 7-http_express.js

--- Node_JS_basic/7-http_express.js
+++ Node_JS_basic/7-http_express.js
@@ -1,23 +1,24 @@
+// 7-http_express.js
 const express = require('express');
 const countStudents = require('./3-read_file_async');
-const databasePath = process.argv[2];
 
+const databasePath = process.argv[2];
 const app = express();
 
 app.get('/', (req, res) => {
-    res.set('Content-Type', 'text/plain');
-    res.send('Hello Holberton School!');
+  res.type('text');
+  res.send('Hello Holberton School!');
 });
 
-app.get('/students', async (req, res) => {
-    res.set('Content-Type', 'text/plain');
-    res.write('This is the list of our students\n');
-    try {
-        const summary = await countStudents(databasePath);
-        res.send(summary);
-    } catch (error) {
-        res.send('Cannot load the database');
-    }
+app.get('/students', (req, res) => {
+  res.type('text');
+  countStudents(databasePath)
+    .then((summary) => {
+      res.send(`This is the list of our students\n${summary}`);
+    })
+    .catch(() => {
+      res.send('This is the list of our students\nCannot load the database');
+    });
 });
 
 app.listen(1245);
