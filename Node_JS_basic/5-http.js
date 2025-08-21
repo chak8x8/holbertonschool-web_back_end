3659f8f2385f5f8275d6459280c05d55a7b8f6f8
chak8x8 <54473526+chak8x8@users.noreply.github.com>
2025-08-21 18:43:57 +1000

    Update 5-http.js

--- Node_JS_basic/5-http.js
+++ Node_JS_basic/5-http.js
@@ -1,27 +1,34 @@
+// 5-http.js
 const http = require('http');
 const countStudents = require('./3-read_file_async');
+
 const databasePath = process.argv[2];
 
-const app = http.createServer(async (req, res) => {
-    res.setHeader('Content-Type', 'text/plain');
-    if (req.url === '/') {
+const app = http.createServer((req, res) => {
+  res.setHeader('Content-Type', 'text/plain');
+
+  if (req.url === '/') {
+    res.statusCode = 200;
+    res.end('Hello Holberton School!');
+    return;
+  }
+
+  if (req.url === '/students') {
+    countStudents(databasePath)
+      .then((summary) => {
         res.statusCode = 200;
-        res.end('Hello Holberton School!');
-        return;
-    }
-    if (req.url === '/students') {
+        res.end(`This is the list of our students\n${summary}`);
+      })
+      .catch(() => {
+        // Spec expects 200 with header + error line
         res.statusCode = 200;
-        res.write('This is the list of our students\n');
-        try {
-            const summary = await countStudents(databasePath);
-            res.end(summary);
-        } catch (err) {
-            res.end('Cannot load the database');
-        }
-        return;
-    }
-    res.statusCode = 404;
-    res.end('Not found');
+        res.end('This is the list of our students\nCannot load the database');
+      });
+    return;
+  }
+
+  res.statusCode = 404;
+  res.end('Not found');
 });
 
 app.listen(1245);
